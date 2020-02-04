0cbae79c8babf05e9b1f1db26428934849498642
rg3915 <rg3915@yahoo.com.br>
2020-02-04 20:30:08 -0300

    Retornando dados fake

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <Customers/>
+    <Customers :customers="customers" />
   </div>
 </template>
-
 <script>
 import Customers from './components/Customers.vue'
 
@@ -12,10 +11,24 @@ export default {
   name: 'app',
   components: {
     Customers
+  },
+  data() {
+    return {
+      customers: [{
+          'id': 1,
+          'first_name': 'Regis',
+          'last_name': 'Santos',
+        },
+        {
+          'id': 2,
+          'first_name': 'Ronaldo',
+          'last_name': 'Silva',
+        },
+      ]
+    }
   }
 }
 </script>
-
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
--- frontend/src/components/Customers.vue
+++ frontend/src/components/Customers.vue
@@ -1,16 +1,31 @@
 <template>
+  <div>
     <h1>Clientes</h1>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Nome</th>
+          <th>Sobrenome</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in customers" :key="item.id">
+          <td>{{ item.first_name }}</td>
+          <td>{{ item.last_name }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
-
 <script>
-  export default {
-    name: 'Customers',
-  }
+// import axios from 'axios'
 
+// const endpoint = 'http://localhost:8000'
+
+export default {
+  name: 'Customers',
+  props: ['customers'],
+}
 </script>
-
-
 <style>
-    
-
 </style>
